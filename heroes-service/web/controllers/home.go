acda94f49bbf5060e7790ff6b9cc620d804cc180
saleelfarooq <saleelfarooq97@gmail.com>
2019-01-30 05:37:31 +0530

    Initial commit

---
+++ heroes-service/web/controllers/home.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"net/http"
+	"encoding/json"
+	"fmt"
+)
+
+func (app *Application) HomeHandler(w http.ResponseWriter, r *http.Request) {
+	helloValue, err := app.Fabric.QueryHello()
+	if err != nil {
+		http.Error(w, "Unable to query the blockchain", 500)
+	}
+
+	type HelloData struct {
+		Key    string `json:"key"`
+		Record struct{
+			ItemName   string `json:"itemname"`
+			NameOfPerson string `json:"nameofperson"`
+			SellingValue string `json:"Sellingvalue"`
+	}
+   }
+	var data []HelloData
+	json.Unmarshal([]byte(helloValue), &data)
+	fmt.Println(data)
+
+	returnData := &struct {
+		ResponseData []HelloData
+	}{
+		ResponseData: data,
+	}
+
+	renderTemplate(w, r, "home.html", returnData)
+}
---
+++ heroes-service/web/controllers/request.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"net/http"
+	"encoding/json"
+)
+type Data struct {
+	//Key   string `json:"key"`
+	ItemName  string `json:"itemname"`
+	NameOfPerson string `json:"nameofperson"`
+	SellingValue string `json:"sellingvalue"`
+}
+
+func (app *Application) RequestHandler(w http.ResponseWriter, r *http.Request) {
+	data := &struct {
+		TransactionId string
+		Success       bool
+		Response      bool
+	}{
+		TransactionId: "",
+		Success:       false,
+		Response:      false,
+	}
+	if r.FormValue("submitted") == "true" {
+		key := r.FormValue("key")
+		productData := Data{}
+		productData.ItemName = r.FormValue("itemname")
+		productData.NameOfPerson = r.FormValue("nameofperson")
+		productData.SellingValue = r.FormValue("sellingvalue")
+        RequestData, _ := json.Marshal(productData)
+		txid, err := app.Fabric.InvokeHello(key,string(RequestData))
+		if err != nil {
+			http.Error(w, "Unable to invoke hello in the blockchain", 500)
+		}
+		data.TransactionId = txid
+		data.Success = true
+		data.Response = true
+	}
+	renderTemplate(w, r, "request.html", data)
+}
